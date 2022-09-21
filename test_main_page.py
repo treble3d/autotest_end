7cd6eec86bbdca4a5f7bf794dfd958f28279e747
treble3d <sviathut@gmail.com>
2022-09-21 15:13:53 +0300

    changed for Page Object

--- test_main_page.py
+++ test_main_page.py
@@ -1,9 +1,10 @@
+from .pages.main_page import MainPage
 from selenium.webdriver.common.by import By
 
 # pytest -v --tb=line --language=en test_main_page.py
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
-    browser.get(link)
-    login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
-    login_link.click()
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_login_page()
